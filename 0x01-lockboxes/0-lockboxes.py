7d8c39bb801e94629e521daeea3ad739ac41bc57
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-04 07:27:35 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opens = set()
+    keys = [0]
+
+    while keys:
+        key = keys.pop()
+        if key not in opens:
+            opens.add(key)
+            for new_key in boxes[key]:
+                if new_key < n:
+                    keys.append(new_key)
+    
+    return len(opens) == n
+
