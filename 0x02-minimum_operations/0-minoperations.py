317d8bdccf9139126a0b25e8616a3f54af2864e7
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-09 18:32:50 +0100

    0x02-minimum_operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+calculates the fewest number of operations needed .
+"""
+
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations needed .
+    """
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
